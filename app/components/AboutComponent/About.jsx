0a1a8c4fb3e3e562928dd759935f4a9124218810
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-03 17:45:28 +0530

    Image Div GSAp Animation Added

--- app/components/AboutComponent/About.jsx
+++ app/components/AboutComponent/About.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { use, useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -9,17 +9,19 @@ gsap.registerPlugin(ScrollTrigger);
 const About = () => {
   const textRef = useRef(null);
   const headingsRef = useRef([]);
+  const imagedivRef = useRef(null);
+  const imageRef = useRef(null);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       gsap.fromTo(
         headingsRef.current,
-        { x: "-100%", opacity: 0 }, 
+        { x: "-100%", opacity: 0 },
         {
           x: "0%",
           opacity: 1,
           duration: 1.5,
-          stagger: 0.3, 
+          stagger: 0.3,
           ease: "power3.out",
         }
       );
@@ -45,26 +47,48 @@ const About = () => {
           },
         }
       );
+
+      if (window.innerWidth <= 768) {
+        gsap.to([imagedivRef.current, imageRef.current], {
+          borderRadius: "40px 40px 40px 40px",
+          scale: 1.2,
+          scrollTrigger: {
+            trigger: imagedivRef.current,
+            start: "top 65%",
+            end: "top 45%",
+            scrub: 1,
+          },
+        });
+      }
     }
   }, []);
 
   return (
     <div className="ag-about-container">
       <div className="ag-about-text">
-        <h1 className="ag-about-text-1" ref={(el) => (headingsRef.current[0] = el)}>
+        <h1
+          className="ag-about-text-1"
+          ref={(el) => (headingsRef.current[0] = el)}
+        >
           Code.
         </h1>
-        <h1 className="ag-about-text-2" ref={(el) => (headingsRef.current[1] = el)}>
+        <h1
+          className="ag-about-text-2"
+          ref={(el) => (headingsRef.current[1] = el)}
+        >
           Create.
         </h1>
-        <h1 className="ag-about-text-3" ref={(el) => (headingsRef.current[2] = el)}>
+        <h1
+          className="ag-about-text-3"
+          ref={(el) => (headingsRef.current[2] = el)}
+        >
           Elevate.
         </h1>
       </div>
 
       <div className="ag-about-info">
-        <div className="ag-about-info-img">
-          <img src="Hero-Banner-Code.jpeg" alt="" />
+        <div className="ag-about-info-img" ref={imagedivRef}>
+          <img src="Hero-Banner-Code.jpeg" ref={imageRef} alt="" />
         </div>
 
         <div className="ag-about-info-text">
@@ -74,9 +98,9 @@ const About = () => {
             tell a story, evoke emotions, and create memorable interactions.
             <br />
             <br />
-            Every element, from the smallest detail to the overall design,
-            plays a role in shaping how users feel and engage, transforming a
-            simple visit into an inspiring journey.
+            Every element, from the smallest detail to the overall design, plays
+            a role in shaping how users feel and engage, transforming a simple
+            visit into an inspiring journey.
           </p>
         </div>
       </div>
