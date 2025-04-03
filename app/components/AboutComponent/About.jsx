9e2ec0a7662aeea6bbedf5295c51ab140fa23f7b
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-03 11:51:45 +0530

    Animation Added in P tag using GSAP

--- app/components/AboutComponent/About.jsx
+++ app/components/AboutComponent/About.jsx
@@ -1,6 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const words = textRef.current.innerHTML.split(" ");
+      textRef.current.innerHTML = words
+        .map((word) => `<span class="word">${word}</span>`)
+        .join(" ");
+
+      const wordElements = gsap.utils.toArray(".word");
+
+      gsap.fromTo(
+        wordElements,
+        { color: "var(--hover-color)" },
+        {
+          color: "var(--text-color)",
+          stagger: 0.1, 
+          scrollTrigger: {
+            trigger: textRef.current,
+            start: "top 80%",
+            end: "top 30%",
+            scrub: true,
+          },
+        }
+      );
+    }
+  }, []);
+
   return (
     <div className="ag-about-container">
       <div className="ag-about-text">
@@ -15,13 +49,15 @@ const About = () => {
         </div>
 
         <div className="ag-about-info-text">
-          <p>
+          <p ref={textRef}>
             Websites are more than just functional spaces—they're immersive
             experiences where visuals, typography and motion come together to
-            tell a story, evoke emotions and create memorable interactions.{" "}
-            <br></br> <br></br> Every element, from the smallest detail to the
-            overall design plays a role in shaping how users feel and engage
-            transforming a simple visit into an inspiring journey.
+            tell a story, evoke emotions and create memorable interactions.
+            <br />
+            <br />
+            Every element, from the smallest detail to the overall design,
+            plays a role in shaping how users feel and engage, transforming a
+            simple visit into an inspiring journey.
           </p>
         </div>
       </div>
