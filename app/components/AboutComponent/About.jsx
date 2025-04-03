3764326888c9679625ea07a83f6d97fdd2fc0ca7
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-03 12:07:11 +0530

    H1 s aniumate on 1st reload

--- app/components/AboutComponent/About.jsx
+++ app/components/AboutComponent/About.jsx
@@ -8,9 +8,22 @@ gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   const textRef = useRef(null);
+  const headingsRef = useRef([]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
+      gsap.fromTo(
+        headingsRef.current,
+        { x: "-100%", opacity: 0 }, 
+        {
+          x: "0%",
+          opacity: 1,
+          duration: 1.5,
+          stagger: 0.3, 
+          ease: "power3.out",
+        }
+      );
+
       const words = textRef.current.innerHTML.split(" ");
       textRef.current.innerHTML = words
         .map((word) => `<span class="word">${word}</span>`)
@@ -23,7 +36,7 @@ const About = () => {
         { color: "var(--hover-color)" },
         {
           color: "var(--text-color)",
-          stagger: 0.1, 
+          stagger: 0.1,
           scrollTrigger: {
             trigger: textRef.current,
             start: "top 80%",
@@ -38,9 +51,15 @@ const About = () => {
   return (
     <div className="ag-about-container">
       <div className="ag-about-text">
-        <h1 className="ag-about-text-1">Code.</h1>
-        <h1 className="ag-about-text-2">Create.</h1>
-        <h1 className="ag-about-text-3">Elevate.</h1>
+        <h1 className="ag-about-text-1" ref={(el) => (headingsRef.current[0] = el)}>
+          Code.
+        </h1>
+        <h1 className="ag-about-text-2" ref={(el) => (headingsRef.current[1] = el)}>
+          Create.
+        </h1>
+        <h1 className="ag-about-text-3" ref={(el) => (headingsRef.current[2] = el)}>
+          Elevate.
+        </h1>
       </div>
 
       <div className="ag-about-info">
@@ -51,8 +70,8 @@ const About = () => {
         <div className="ag-about-info-text">
           <p ref={textRef}>
             Websites are more than just functional spaces—they're immersive
-            experiences where visuals, typography and motion come together to
-            tell a story, evoke emotions and create memorable interactions.
+            experiences where visuals, typography, and motion come together to
+            tell a story, evoke emotions, and create memorable interactions.
             <br />
             <br />
             Every element, from the smallest detail to the overall design,
