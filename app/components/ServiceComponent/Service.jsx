b5d84ef355c68467d95ff976e405ca3302ce9a70
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-04 17:07:58 +0530

    Services Added

--- app/components/ServiceComponent/Service.jsx
+++ app/components/ServiceComponent/Service.jsx
@@ -1,13 +1,18 @@
 "use client";
 import { useEffect, useRef } from "react";
-import { FaWordpress, FaReact, FaShopify } from "react-icons/fa6";
+import { FaWordpress, FaReact, FaShopify,FaNodeJs } from "react-icons/fa6";
 import { RiNextjsFill } from "react-icons/ri";
+import { AiOutlineHtml5 } from "react-icons/ai";
 import { DiGoogleAnalytics } from "react-icons/di";
 import { SiMongodb, SiExpress } from "react-icons/si";
 import { VscGithubInverted } from "react-icons/vsc";
 import { FaGitAlt } from "react-icons/fa";
 import { FaHtml5 } from "react-icons/fa6";
 import { FaCss3 } from "react-icons/fa6";
+import { SiHostinger } from "react-icons/si";
+import { IoLogoVercel } from "react-icons/io5";
+import { SiGodaddy } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -59,6 +64,26 @@ const Service = () => {
   return (
     <div className="ag-service-container" ref={containerRef}>
       <div className="ag-service">
+        <AiOutlineHtml5 className="service-icon" />
+      </div>
+      <div className="ag-service">
+        <FaCss3 className="service-icon" />
+      </div>
+      <div className="ag-service">
+        <FaNodeJs className="service-icon" />
+      </div>
+      <div className="ag-service">
+        <SiExpress className="service-icon" />
+      </div>
+
+      <div className="ag-service">
+        <SiMongodb className="service-icon" />
+      </div>
+      <div className="ag-service">
+        <RiNextjsFill className="service-icon" />
+      </div>
+
+      <div className="ag-service">
         <FaReact className="service-icon" />
       </div>
       <div className="ag-service">
@@ -67,21 +92,14 @@ const Service = () => {
       <div className="ag-service">
         <FaShopify className="service-icon" />
       </div>
-      <div className="ag-service">
-        <RiNextjsFill className="service-icon" />
-      </div>
-      <div className="ag-service">
-        <RiNextjsFill className="service-icon" />
-      </div>
+      
       <div className="ag-service">
         <DiGoogleAnalytics className="service-icon" />
       </div>
-      <div className="ag-service">
-        <SiMongodb className="service-icon" />
-      </div>
-      <div className="ag-service">
-        <SiExpress className="service-icon" />
-      </div>
+      
+     
+
+      
 
       <div className="ag-service">
         <VscGithubInverted className="service-icon" />
@@ -91,12 +109,22 @@ const Service = () => {
         <FaGitAlt className="service-icon" />
       </div>
 
+      
+
+      
+
       <div className="ag-service">
-        <FaHtml5 className="service-icon" />
+        <SiHostinger className="service-icon" />
       </div>
 
       <div className="ag-service">
-        <FaCss3 className="service-icon" />
+        <IoLogoVercel className="service-icon" />
+      </div>
+      <div className="ag-service">
+        <SiGodaddy className="service-icon" />
+      </div>
+      <div className="ag-service">
+        <FaAws className="service-icon" />
       </div>
     </div>
   );
