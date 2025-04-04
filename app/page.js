6cfe2c47747e393be2b98dd7e4a2dc5cbce2b7af
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-04 15:38:20 +0530

    Services Component Adde

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/AboutComponent/About";
+import Service from "./components/ServiceComponent/Service";
 
 export default function Home() {
   return (
@@ -8,6 +9,10 @@ export default function Home() {
       <Header />
       <section id="about" className="section">
         <About />
+      </section>
+
+      <section id="service" className="section">
+        <Service />
       </section>
       <section id="experience" className="section">
         Experience
