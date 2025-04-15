6809cb56807395d88153c79d8f38efd2d7decaed
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-15 16:18:55 +0530

    Project Showcase animation added

--- app/components/ProjectComponent/Projects.jsx
+++ app/components/ProjectComponent/Projects.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+'use client'
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
+  const imagesRef = useRef([]);
+
+  useEffect(() => {
+    imagesRef.current.forEach((img, index) => {
+      gsap.from(img, {
+        y: 100,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: img,
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      });
+    });
+  }, []);
+
+  const projectImages = [
+    { src: "Uniqaya-mockup.jpg", alt: "Uniqaya Project" },
+    { src: "Uniqaya-mockup.jpg", alt: "Another Project" },
+    { src: "Uniqaya-mockup.jpg", alt: "Creative Work" },
+  ];
+
   return (
     <div className="ag-projects-container">
       <div className="ag-projects-title">
@@ -11,17 +40,15 @@ const Projects = () => {
       </div>
 
       <div className="ag-projects-content">
-        <div className="ag-projects">
-          <img src="Uniqaya-mockup.jpg" alt="Project 1" />
-        </div>
-
-        <div className="ag-projects">
-          <img src="Uniqaya-mockup.jpg" alt="Project 2" />
-        </div>
-
-        <div className="ag-projects">
-          <img src="Uniqaya-mockup.jpg" alt="Project 3" />
-        </div>
+        {projectImages.map((project, index) => (
+          <div className="ag-projects" key={index}>
+            <img
+              src={project.src}
+              alt={project.alt}
+              ref={(el) => (imagesRef.current[index] = el)}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
